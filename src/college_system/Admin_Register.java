868a3fdf30d7ad1f9047d3a8b28d4ae41b62fd14
Ajeet Kumar Maurya <ajeetkumar99190@gmail.com>
2018-08-21 15:04:36 +0530

    Update Admin_Register.java

--- src/college_system/Admin_Register.java
+++ src/college_system/Admin_Register.java
@@ -343,7 +343,7 @@ public class Admin_Register extends javax.swing.JFrame {
         Session session = Session.getDefaultInstance(props,
                 new javax.mail.Authenticator() {
                 protected PasswordAuthentication getPasswordAuthentication(){
-                return new PasswordAuthentication("ajeetkumar99190@gmail.com","9598725268");
+                return new PasswordAuthentication("ajeetkumar99190@gmail.com","password");
                 }        
                 }  
                 );
