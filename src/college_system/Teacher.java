08a48b9bfb51e52fd781f5d3531d6a57156d61fc
Ajeet Kumar Maurya <ajeetkumar99190@gmail.com>
2018-08-21 15:08:36 +0530

    Update Teacher.java

--- src/college_system/Teacher.java
+++ src/college_system/Teacher.java
@@ -1246,7 +1246,7 @@ public class Teacher extends javax.swing.JFrame {
             Session session = Session.getDefaultInstance(props,
                 new javax.mail.Authenticator() {
                     protected PasswordAuthentication getPasswordAuthentication(){
-                        return new PasswordAuthentication("ajeetkumar99190@gmail.com","9598725268");
+                        return new PasswordAuthentication("ajeetkumar99190@gmail.com","password");
                     }
                 }
             );
